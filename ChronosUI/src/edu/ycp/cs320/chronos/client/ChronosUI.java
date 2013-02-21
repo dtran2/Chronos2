75afdd4834406b766df2008ad2138be4dc98ce86
Dung Tran <dtran2@ycp.edu>
2013-02-21 16:22:50 -0500

    initial checkin of GWT project

---
+++ ChronosUI/src/edu/ycp/cs320/chronos/client/ChronosUI.java
@@ -0,0 +1,14 @@
+package edu.ycp.cs320.chronos.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class ChronosUI implements EntryPoint {
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+	}
+}
