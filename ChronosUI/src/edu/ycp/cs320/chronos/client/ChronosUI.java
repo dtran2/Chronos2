9776fa4944507e4546dbd3a4a696dddf3a6ad796
Dung Tran <dtran2@ycp.edu>
2013-02-21 16:39:19 -0500

    initialized ChronosUI.html

--- ChronosUI/src/edu/ycp/cs320/chronos/client/ChronosUI.java
+++ ChronosUI/src/edu/ycp/cs320/chronos/client/ChronosUI.java
@@ -1,6 +1,7 @@
 package edu.ycp.cs320.chronos.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.*;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
