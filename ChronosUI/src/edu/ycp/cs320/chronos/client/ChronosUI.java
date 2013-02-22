8fc0803bc88410a34c3068b0f8c34c0964d81be8
Chris Davis <cdavis17@ycp.edu>
2013-02-22 13:50:25 -0500

    Add to ChronosUI to set up Sign In Panel

--- ChronosUI/src/edu/ycp/cs320/chronos/client/ChronosUI.java
+++ ChronosUI/src/edu/ycp/cs320/chronos/client/ChronosUI.java
@@ -9,21 +9,36 @@ import com.google.gwt.event.logical.shared.ValueChangeEvent;
 
 
 public class ChronosUI implements EntryPoint{
-	private TextBox email = new TextBox();
-	private PasswordTextBox pwd = new PasswordTextBox();
+	private VerticalPanel VerticalPanel = new VerticalPanel();
+	private HorizontalPanel HorizontalPanel = new HorizontalPanel();
+	private TextBox pwd = new TextBox();
 	private TextBox userID = new TextBox();
+	private Button Login = new Button("Login");
+	private Button SignUpNow = new Button("SIGN UP NOW!");
+	
 	@Override
 	public void onModuleLoad() {
 		
-		email.setText("E-mail");
-		email.setName("email");
-		email.setAlignment(TextAlignment.CENTER);
-		pwd.setText("password");
-		pwd.setName("password");
-		pwd.setAlignment(TextAlignment.CENTER);
-		userID.setText("Enter ID");
-		userID.setName("User ID");
-		userID.setAlignment(TextAlignment.CENTER);
+		
+		VerticalPanel.add(SignUpNow);
+		VerticalPanel.setCellHorizontalAlignment(SignUpNow, HasHorizontalAlignment.ALIGN_RIGHT);
+		userID.setTextAlignment(TextBoxBase.ALIGN_CENTER);
+		userID.setText("Enter User ID");
+		userID.setName("Enter User ID");
+		VerticalPanel.add(userID);
+		VerticalPanel.setCellHorizontalAlignment(userID, HasHorizontalAlignment.ALIGN_CENTER);
+		pwd.setText("Password");
+		pwd.setTextAlignment(TextBoxBase.ALIGN_CENTER);
+		VerticalPanel.add(pwd);
+		VerticalPanel.setCellHorizontalAlignment(pwd, HasHorizontalAlignment.ALIGN_CENTER);
+		VerticalPanel.add(Login);
+		VerticalPanel.setCellHorizontalAlignment(Login, HasHorizontalAlignment.ALIGN_CENTER);
+		VerticalPanel.setCellHeight(Login, "20");
+		VerticalPanel.setCellWidth(Login, "20");
+		Login.setSize("121px", "35px");
+		SignUpNow.setSize("117px", "32px");
+	
+		
 		
 		// TODO Auto-generated method stub
 		
