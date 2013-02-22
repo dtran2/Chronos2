84ea37585f1caa6e552d66614504dd97cc2cb550
Chris Davis <cdavis17@ycp.edu>
2013-02-22 12:54:44 -0500

    Add to User Interface Class

--- ChronosUI/src/edu/ycp/cs320/chronos/client/ChronosUI.java
+++ ChronosUI/src/edu/ycp/cs320/chronos/client/ChronosUI.java
@@ -11,13 +11,20 @@ import com.google.gwt.event.logical.shared.ValueChangeEvent;
 public class ChronosUI implements EntryPoint{
 	private TextBox email = new TextBox();
 	private PasswordTextBox pwd = new PasswordTextBox();
+	private TextBox userID = new TextBox();
 	@Override
 	public void onModuleLoad() {
 		
 		email.setText("E-mail");
 		email.setName("email");
 		email.setAlignment(TextAlignment.CENTER);
-
+		pwd.setText("password");
+		pwd.setName("password");
+		pwd.setAlignment(TextAlignment.CENTER);
+		userID.setText("Enter ID");
+		userID.setName("User ID");
+		userID.setAlignment(TextAlignment.CENTER);
+		
 		// TODO Auto-generated method stub
 		
 	}
