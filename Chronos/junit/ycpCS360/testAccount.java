7bdb2f96e9be849a4141a3384bf6091c9bd0d58a
Chris Davis <cdavis17@ycp.edu>
2013-02-22 12:32:06 -0500

    Added testAccount and testEvent frames

---
+++ Chronos/junit/ycpCS360/testAccount.java
@@ -0,0 +1,8 @@
+package ycpCS360;
+import edu.ycp.cs320.chronos.database.Account;
+
+import junit.framework.TestCase;
+
+public class testAccount extends TestCase {
+
+}
---
+++ Chronos/junit/ycpCS360/testEvent.java
@@ -0,0 +1,8 @@
+package ycpCS360;
+import edu.ycp.cs320.chronos.database.Event;
+
+import junit.framework.TestCase;
+
+public class testEvent extends TestCase {
+
+}
