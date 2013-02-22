844363cae6da95489a0796e6c73b9099a4134b24
Chris Davis <cdavis17@ycp.edu>
2013-02-22 09:24:26 -0500

    Added testAccount and testIDatabase junit classes

--- junit/ycpCS360/testAccount.java
+++ junit/ycpCS360/testAccount.java
@@ -14,6 +14,7 @@ import ycpCS320.FakeDatabase;
 
 public class testAccount extends TestCase {
 	HashMap<String, Account> accountMap = new HashMap<String, Account>();
+	
 	private FakeDatabase fd;
 	public void setUp(){
 		fd = new FakeDatabase();
