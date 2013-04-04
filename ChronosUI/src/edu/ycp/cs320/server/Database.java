c7df6a5560c7831726e8d743888fedc3834d7ba9
Dung Tran <dtran2@ycp.edu>
2013-04-04 17:45:54 -0400

    -Could not pull changes made in repository cdavis17 -Copied and pasted new classes from cdavis17's github repositories

---
+++ ChronosUI/src/edu/ycp/cs320/server/Database.java
@@ -0,0 +1,124 @@
+package edu.ycp.cs320.server;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Map;
+
+import com.google.gwt.dev.util.collect.HashMap;
+import com.sun.corba.se.pept.transport.Connection;
+
+import edu.ycp.cs320.chronos.shared.Account;
+import edu.ycp.cs320.chronos.shared.Event;
+import edu.ycp.cs320.chronos.shared.IDatabase;
+
+public class Database implements IDatabase {
+	private Map<String, Account> accountMap;
+
+	@Override
+	public Event findEvent(String eventName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+	@Override
+	public void createAccount(String usrName, String password, String email) {
+
+		return databaseRun(new ITransaction<Map<String, Account>>() {
+
+			public Map<String, Account> run(Connection conn) throws SQLException {
+				PreparedStatement stmt = null;
+				ResultSet resultSet = null;
+
+				try {
+					stmt = conn.prepareStatement("select * from accounts");
+					resultSet = stmt.executeQuery();
+
+					Map<String, Account> result = new HashMap<String, Account>();
+
+					while (resultSet.next()) {
+						Account account = new Account(usrName, password, email);
+
+						account.setId(resultSet.getInt(1));
+						account.setUserName(resultSet.getString(2));
+						account.setPassword(resultSet.getString(3));
+						account.setEmail(resultSet.getString(4));
+
+						result.add(account);
+					}
+
+					return result;
+				} finally {
+					DatabaseUtil.closeQuietly(stmt);
+					DatabaseUtil.closeQuietly(resultSet);
+				}
+			}
+		});
+
+	}
+
+
+	@Override
+	public boolean verifyAccount(String usrName, String password) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Event getNextEvent(Account user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+	@Override
+	public int getMonth(String eventName) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+
+	@Override
+	public int getDay(String eventName) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+
+	@Override
+	public int getYear(String eventName) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+
+	@Override
+	public int getStartTime(String eventName) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+
+	@Override
+	public int getEndTime(String eventName) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+
+	@Override
+	public String getDetails(String eventName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ ChronosUI/src/edu/ycp/cs320/server/IDatabase.java
@@ -0,0 +1,5 @@
+package edu.ycp.cs320.server;
+
+public interface IDatabase {
+
+}
